1b035994de8755b08c23c874b15688ee1c1549fb
Durga prasad <psivadurgaprasad88@gmail.com>
2025-06-26 22:21:51 +0530

    Added frontend code

--- deepsearch-frontend/vite.config.js
+++ deepsearch-frontend/vite.config.js
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // Don't include server.proxy in deployed config
 })
